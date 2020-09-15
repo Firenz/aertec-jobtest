c27077ed8e9c6169d07e98f6f073ff24f6de723f
Alicia G <alicia.guardenoalbertos@gmail.com>
2020-09-15 20:24:18 +0200

    add movie details component & container

---
+++ src/pods/movie-details/movie-details.container.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { MovieDetailsComponent } from './movie-details.component';
+
+export const MovieDetailsContainer: React.FC = () => {
+  return <MovieDetailsComponent />;
+};
